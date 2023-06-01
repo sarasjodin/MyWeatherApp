fbdab372dfaea16d0210ae0bc79ffcd4c61f6368
La svedese <5554930+sarasjodin@users.noreply.github.com>
2023-06-01 08:09:35 +0200

    setAttributes for img instead of using innerHTML

--- src/index.js
+++ src/index.js
@@ -15,27 +15,29 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  let iconElement = document.querySelector("#weather-icon img");
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let dateElement = document.querySelector("#date");
   let descriptionElement = document.querySelector("#description");
   let precipitationElement = document.querySelector("#precipitation");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#weather-icon");
+
+  let iconCode = response.data.weather[0].icon;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${iconCode}@4x.png`
+  );
+  iconElement.setAttribute("alt", `${descriptionElement}`);
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   descriptionElement.innerHTML = response.data.weather[0].description;
   precipitationElement.innerHTML = response.data.clouds.all;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.innerHTML = response.data.weather[0].icon;
-  let iconCode = response.data.weather[0].icon;
-  iconElement.innerHTML = `<img src="http://openweathermap.org/img/wn/${iconCode}@4x.png" alt="${descriptionElement}"/>`;
-
-  console.log(response.data);
 }
 
 let apiKey = "ff1d9ea9376b5c27a82e04fc2b2abdbb";
